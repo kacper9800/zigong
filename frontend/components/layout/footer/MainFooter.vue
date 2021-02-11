5e36655d89fcf4037718f1331b8c86d5195b9ecb
mateusz-rozkosz-rmtcfm-com <mateusz.rozkosz@remotecraftsmen.com>
2021-02-11 22:36:48 +0100

    home page design - slider need to be fix

--- frontend/components/layout/footer/MainFooter.vue
+++ frontend/components/layout/footer/MainFooter.vue
@@ -1,13 +1,44 @@
 <template>
-  
+    <footer class="footer gray-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <p>
+                        16504 Aldine Westfield Rd., Bldg. A | Houston, TX 77032
+                    </p>
+                    phone: 281-987-1001 fax: 281-987-1002
+                </div>
+
+                <div class="col">
+                    <p>Chardon Road | Willoughby Hills, OH 44094</p>
+                    phone: 440-269-8160 fax: 440-269-8174
+                </div>
+
+                <div class="col">
+                    <p>
+                        Email:
+                        <a href="mailto:info@zim-llc.com">info@zim-llc.com</a>
+                        <br />
+                        © Zigong International Marketing, LLC
+                    </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4">
+                    <a class="backtop js-scroll-trigger reveal" href="#top">
+                        <i class="icofont-long-arrow-up"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
 
 <script>
-  export default {
-    name: "Footer"
-  };
+export default {
+    name: 'Footer'
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
