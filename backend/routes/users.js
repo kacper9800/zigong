b6824fe411fd3f7d9e5c7ad0867d0c8a6269431c
Kacper Rzymkiewicz <kacper9800@gmail.com>
2021-02-12 14:39:10 +0100

    Swagger Open Api Documentations

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,87 @@
+/**
+ * @swagger
+ * /users/{userId}:
+ *  get:
+ *   summary: Retrieve a single user by ID
+ *   description: Retrieve a single user by ID
+ *   parameters:
+ *     -in: path
+ *     name: id
+ *     type: integer
+ *     required: true
+ *     description: Numeric ID of the user to get.
+ *   responses:
+ *    200:
+ *      description: A single user
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/User'
+ *
+ * /users:
+ *  get:
+ *   summary: Retrieve a list of users
+ *   description: Retrieve a list of registered users in application
+ *   responses:
+ *    200:
+ *      description: A list of users
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: array
+ *            items:
+ *              $ref: '#/components/schemas/User'
+ *  post:
+ *   summary: Create user
+ *   responses:
+ *    201:
+ *      description: Created
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/User'
+ * @swagger
+ * components:
+ *   schemas:
+ *    User:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: integer
+ *          description: The user ID
+ *          example: 15
+ *        email:
+ *          type: string
+ *          description: The user email
+ *          example: martin@gmail.com
+ *        dayOfBirth:
+ *          type: date
+ *          description: The user date of birth
+ *          example: 20.01.1993
+ *        password:
+ *          type: string
+ *          description: The user password
+ *        firstName:
+ *          type: string
+ *          description: The user name
+ *          example: John
+ *        lastName:
+ *          type: string
+ *          description: The user surname
+ *          example: Smith
+ *        availableDays:
+ *          type: integer
+ *          description: The user account available days
+ *          example: 15
+ */
+
 const express = require("express");
 const router = express.Router();
 const {
   isLoggedIn,
   isAdmin,
   adminIsNotRequired,
-  validate,
+  validate
 } = require("../middleware");
 const usersValidator = require("../validators/usersValidator");
 
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const config = require("./config");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const di = require("./di");
 
@@ -24,3 +26,37 @@ const routes = require("./routes")(di);
 app.use(routes);
 
 module.exports = server;
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Zigong Application - Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "Zigong application. Created by M & K",
+      // license: {
+      //   name: "MIT",
+      //   url: "https://spdx.org/licenses/MIT.html",
+      // },
+      // contact: {
+      //   name: "LogRocket",
+      //   url: "https://logrocket.com",
+      //   email: "info@email.com",
+      // },
+    },
+    servers: [
+      {
+        url: "http://localhost:3004/api-docs",
+      },
+    ],
+  },
+  apis: ["./routes/users.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
