36cc541a126b45a91a5b5a0fee386db378bf02d0
mateusz-rozkosz-rmtcfm-com <mateusz.rozkosz@remotecraftsmen.com>
2021-02-12 16:08:26 +0100

    cleaning - swagger

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const config = require("./config");
-const swaggerJsdoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
 
 const di = require("./di");
 
@@ -21,42 +18,8 @@ app.use(express.static("./public"));
 
 const server = require("http").createServer(app);
 
-const routes = require("./routes")(di);
+const routes = require("./routes")(di, app);
 
 app.use(routes);
 
 module.exports = server;
-
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Zigong Application - Express API with Swagger",
-      version: "0.1.0",
-      description:
-        "Zigong application. Created by M & K",
-      // license: {
-      //   name: "MIT",
-      //   url: "https://spdx.org/licenses/MIT.html",
-      // },
-      // contact: {
-      //   name: "LogRocket",
-      //   url: "https://logrocket.com",
-      //   email: "info@email.com",
-      // },
-    },
-    servers: [
-      {
-        url: "http://localhost:3004/api-docs",
-      },
-    ],
-  },
-  apis: ["./routes/users.js"],
-};
-
-const specs = swaggerJsdoc(options);
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(specs)
-);
