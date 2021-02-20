a9a31fbe034c19b917fbe4aad4eff4b7fd706632
Kacper Rzymkiewicz <kacper9800@gmail.com>
2021-02-20 12:37:14 +0100

    Admin panel -> Categories layout

--- frontend/components/layout/sider/AdminSider.vue
+++ frontend/components/layout/sider/AdminSider.vue
@@ -1,75 +1,59 @@
 <template>
-    <a-layout-sider width="200">
-        <a-menu
-            mode="inline"
+  <a-layout-sider width="200">
+    <a-menu mode="inline"
             :open-keys.sync="openKeys"
-            :style="{ height: '80vh', borderRight: 0 }"
-        >
-            <a-sub-menu v-for="page in pages" :key="page.name">
+            :style="{ height: '80vh', borderRight: 0}">
+      <a-sub-menu v-for="page in pages" :key="page.name">
                 <span slot="title"
-                    ><a-icon :type="page.icon" />
-                    <nuxt-link :to="mainPath + page.path">
+                ><a-icon :type="page.icon"/>
+                    <nuxt-link :to="mainPath + page.path" :style="{ color: '#111111'}">
                         {{ page.name }}
                     </nuxt-link>
                 </span>
-                <a-menu-item
-                    v-for="subPage in page.subPages"
-                    :key="subPage.name"
-                >
-                    <nuxt-link :to="mainPath + page.path + subPage.path">
-                        {{ subPage.name }}
-                    </nuxt-link>
-                </a-menu-item>
-            </a-sub-menu>
+        <a-menu-item
+          v-for="subPage in page.subPages"
+          :key="subPage.name">
+          <nuxt-link :to="mainPath + page.path + subPage.path">
+            {{ subPage.name }}
+          </nuxt-link>
+        </a-menu-item>
+      </a-sub-menu>
 
-            <a-menu-item
-                v-for="singlePage in singlePages"
-                :key="singlePage.name"
-            >
-                <nuxt-link :to="mainPath + singlePage.path">
-                    <a-icon :type="singlePage.icon" />
-                    <span>{{ singlePage.name }}</span>
-                </nuxt-link>
-            </a-menu-item>
-        </a-menu>
-    </a-layout-sider>
+      <a-menu-item
+        v-for="singlePage in singlePages"
+        :key="singlePage.name">
+        <nuxt-link :to="mainPath + singlePage.path">
+          <a-icon :type="singlePage.icon"/>
+          <span>{{ singlePage.name }}</span>
+        </nuxt-link>
+      </a-menu-item>
+    </a-menu>
+  </a-layout-sider>
 </template>
 
 <script>
-export default {
-    layout: 'admin',
+  export default {
+    layout: "admin",
     data() {
-        return {
-            openKeys: ['products'],
-            mainPath: '/admin',
-            pages: [
-                {
-                    path: '/about',
-                    name: 'About',
-                    icon: 'appstore',
-                    subPages: [
-                        { path: '/powders', name: 'Powders' },
-                        { path: 'cemented-carbides', name: 'Cemented Carbides' }
-                    ]
-                },
-                {
-                    path: '/products',
-                    name: 'Products',
-                    icon: 'appstore',
-                    subPages: [
-                        { path: '/powders', name: 'Powders' },
-                        { path: 'cemented-carbides', name: 'Cemented Carbides' }
-                    ]
-                }
-            ],
-            singlePages: [
-                { path: '/articles', name: 'Articles', icon: 'file-text' },
-                { path: '/resources', name: 'Resources', icon: 'file' },
-                { path: '/users', name: 'Users', icon: 'team' }
+      return {
+        openKeys: ["products"],
+        mainPath: "/admin",
+        pages: [
+          {
+            path: "/about", name: "About", icon: "appstore", subPages: [
+              { path: "/add", name: "Powders" },
+              { path: "cemented-carbides", name: "Cemented Carbides" }
             ]
-        };
+          }
+        ],
+        singlePages: [
+          { path: "/categories", name: "Categories", icon: "container"},
+          { path: "/products", name: "Products", icon: "appstore" },
+          { path: "/files", name: "Files", icon: "file" },
+          { path: "/users", name: "Users", icon: "team" }
+        ]
+      };
     }
-};
+  };
 </script>
-
 <style scoped></style>
--- frontend/store/category.js
+++ frontend/store/category.js
@@ -1,36 +1,75 @@
-'use strict';
+"use strict";
 
 export const defaultState = () => {
-    return {
-        categories: {}
-    };
+  return {
+    categories: {}
+  };
 };
 
 export const state = () => defaultState();
 
 export const getters = {
-    getCategories: state => state.categories
+  getCategories: state => state.categories,
+  getCategoryById: state => state.categoryById,
+  getCategoryBySlug: state => state.categoryBySlug,
+  getSavedCategory: state => state.savedCategory
 };
 
 export const mutations = {
-    SET_CATEGORIES(state, categories) {
-        state.categories = categories;
-    }
+  SET_CATEGORIES(state, categories) {
+    state.categories = categories;
+  },
+
+  SET_CATEGORY_BY_ID(state, categoryById) {
+    state.categoryById = categoryById;
+  },
+
+  SET_CATEGORY_BY_SLUG(state, categoryBySlug) {
+    state.categoryBySlug = categoryBySlug;
+  },
+
+  SET_SAVED_CATEGORY(state, savedCategory) {
+    state.savedCategory = savedCategory
+  }
 };
 
 export const actions = {
-    async getData({ commit }, lng) {
-        const { data } = await this.$axios.get(`/categories`, { params: lng });
+  async getAllCategories({ commit }, lng) {
+    const { data } = await this.$axios.get(`/categories`, { params: lng });
 
-        commit('SET_CATEGORIES', data);
+    commit("SET_CATEGORIES", data);
 
-        return data;
-    }
+    return data;
+  },
+
+  async getOneById({ commit }, id ) {
+    const { data } = await this.$axios.get(`/categories/${id}`);
+
+    commit("SET_CATEGORY_BY_ID", data);
+
+    return data;
+  },
+
+  async getOneBySlug({ commit }, slug ) {
+    const { data } = await this.$axios.get(`/categories/${slug}`);
+
+    commit("SET_CATEGORY_BY_SLUG", data);
+
+    return data;
+  },
+
+  async createOne({ commit }, { category }) {
+    const { data } = await  this.$axios.post('categories', category);
+
+    commit("SET_SAVED_CATEGORY", data);
+
+    return data;
+  }
 };
 
 export default {
-    state,
-    getters,
-    mutations,
-    actions
+  state,
+  getters,
+  mutations,
+  actions
 };
