a6d9292861cc70e26348cb303fde14e6e8fc7fe8
mateusz-rozkosz-rmtcfm-com <mateusz.rozkosz@remotecraftsmen.com>
2021-02-21 16:09:22 +0100

    cors fix

--- backend/plugins/cors.js
+++ backend/plugins/cors.js
@@ -4,28 +4,17 @@ const config = require("../config");
 module.exports = (app) => {
   const corsSites = config.app.corsSites.split(",").map((site) => site.trim());
   const originsWhitelist = [
-    "http://localhost:3001",
+    "http://localhost:8080",
     config.app.frontendUrl,
-    config.app.adminUrl,
     ...corsSites,
   ];
 
-  console.log(originsWhitelist);
   const corsOptions = {
     origin(origin, callback) {
-      if (originsWhitelist.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
+      callback(null, true);
     },
     credentials: true,
   };
 
   app.use(cors(corsOptions));
-
-  //   app.use(function (err, req, res, next) {
-  //     if (err.message !== "Not allowed by CORS") return next();
-  //     res.status(200).json({ code: 200, message: "Request not allowed by CORS" });
-  //   });
 };
