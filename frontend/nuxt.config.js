07819f03297fb249f2a7b99f422bb5e321ab8da5
mateusz-rozkosz-rmtcfm-com <mateuszrozkosz97@gmail.com>
2021-03-07 20:24:50 +0100

    swiper fix

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -44,7 +44,7 @@ export default {
     ],
 
     // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-    plugins: ['@/plugins/antd-ui', '@/plugins/vuelidate', '@/plugins/vue-cool-lightbox'],
+    plugins: ['@/plugins/vuelidate', '@/plugins/vue-cool-lightbox'],
 
     // Auto import components (https://go.nuxtjs.dev/config-components)
     components: true,
